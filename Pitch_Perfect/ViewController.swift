31ebbd56a28d117cd95ce497b4777746d210384c
政达 何 <Crossbell@zhengdadeMacBook-Pro.local>
2017-01-07 14:56:39 +0800

    Initial Commit

---
+++ Pitch_Perfect/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pitch_Perfect
+//
+//  Created by 政达 何 on 2017/1/7.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
